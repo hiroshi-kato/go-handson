c2b58e66ad9a9845f2699488758febdb31b70792
hiroshi-kato <hiroshikato1208@gmail.com>
2021-12-13 22:57:23 +0900

    勉強会2予習

--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 // main関数からすべてのコードが実行される
 func main() {
-	println("Hello, world!")
-
 	// 複数行コメントは /* */ で指定
 	/*
 		print("Hello, world!")
 	*/
+
+	// 標準出力に出力する
+	fmt.Print("Hello, world!")
+	// 改行あり
+	fmt.Println("Hello, world!")
+
+	// あたりはずれ
+	t := time.Now().Unix()
+	rand.Seed(t)
+	u := rand.Intn(10)
+	if u == 7 {
+		fmt.Println(u)
+		fmt.Println("あたり")
+	} else {
+		fmt.Println(u)
+		fmt.Println("はずれ")
+	}
+
+	// レア度を作ってみる
+	switch u {
+	case 0:
+		fmt.Println(("u is 0"))
+	case 1, 2:
+		fmt.Println(("u is 1 or 2"))
+	}
+
+	// レア度ごとに出る確率を買えてみよう
+	switch {
+	case u == 1:
+		fmt.Println(("uis 1"))
+	case u == 2:
+		fmt.Println(("uis 2"))
+	default:
+		fmt.Println(("default"))
+	}
+
 }
