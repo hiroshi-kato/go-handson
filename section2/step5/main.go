9ac493f6dd31be2d3c2acd1852f7892926696b22
hiroshi-kato <hiroshikato1208@gmail.com>
2021-12-19 15:26:41 +0900

    section2予習

---
+++ section2/step5/main.go
@@ -0,0 +1,33 @@
+// STEP05: キャラクタを表す型を定義しよう（ユーザ定義型）
+
+package main
+
+import "fmt"
+
+// TODO: string型をベースにしたrarity型を定義する
+type rarity string
+
+const (
+	rarityN  rarity = "ノーマル"
+	rarityR  rarity = "R"
+	raritySR rarity = "SR"
+	rarityXR rarity = "XR"
+)
+
+type card struct {
+	rarity rarity // レア度
+	// TODO: 文字列型のフィールドnameを設ける
+	name string
+}
+
+func main() {
+
+	// TODO: rarityフィールドがrarityNで
+	// nameフィールドが"スライム"の変数slimeを定義する
+	slime := card{rarityN, "スライム"}
+
+	fmt.Println(slime)
+
+	dragon := card{rarity: raritySR, name: "ドラゴン"}
+	fmt.Println(dragon)
+}
